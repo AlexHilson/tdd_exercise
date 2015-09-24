dd7daeeff47ad1703c87545f6ff58512caae8420
Alex Hilson <alexhilsonwork@googlemail.com>
2015-09-24 15:50:28 +0100

    Refactored functional tests to use unittest module.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,22 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitor(unittest.TestCase): 
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_start_and_retrieve_list(self):
+        # User visits homepage of to-do app.
+        self.browser.get('http://localhost:8000')
+
+        # They notice that the title and header mention to-do lists.
+        self.assertIn('To-Do', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main()
