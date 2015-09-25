da4150dff8fe5478320affa2013e4b4884f231a6
Alex Hilson <alexhilsonwork@googlemail.com>
2015-09-25 12:20:17 +0100

    Updated homepage view to render post requests.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -20,29 +20,39 @@ class NewVisitor(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
+
         # They are prompted to enter a to-do item.
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
-        
+
         # They type 'Buy peacock feathers' into a text box.
-        todo_item = 'Buy peacock feathers'
-        inputbox.send_keys(todo_item)
-        
+        todo_item1 = 'Buy peacock feathers'
+        inputbox.send_keys(todo_item1)
+
         # When they hit enter, the page updates and the page lists
         # "1. Buy peacock feathers" as an item in a to-do list table.
         inputbox.send_keys(Keys.ENTER)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: %s' % (todo_item) for row in rows),
-            'New to-do item did not appear in table'
-        )
-        
+        self.assertIn('1: %s' % (todo_item1), [row.text for row in rows])
+
         # There is still a text box they can use to add another item. They 
         # enter "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        todo_item2 = 'Use peacock feathers to make a fly'
+        inputbox.send_keys(todo_item2)
+
+        # When they hit enter, the page updates and the page lists
+        # "2. Use peacock feathers to make a fly" as an item in a to-do list table.
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        
+        self.assertIn('1: %s' % (todo_item1), [row.text for row in rows])
+        self.assertIn('2: %s' % (todo_item2), [row.text for row in rows])
         self.fail("Finish the test!")
         
 if __name__ == '__main__':
