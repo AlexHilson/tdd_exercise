684f09b4c8cd1f12a51f12f3aefe7882af539e18
Alex Hilson <alexhilsonwork@googlemail.com>
2015-09-25 16:04:05 +0100

    Created new view, URL and template for displaying lists.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -41,22 +41,12 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/only_list/')
 
     def test_homepage_only_saves_items_when_necessary(self):
         request = HttpRequest()
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_homepage_displays_all_todos(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('item 1', response.content.decode())
-        self.assertIn('item 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -77,3 +67,18 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first list item')
         self.assertEqual(second_saved_item.text, 'The second list item')
+
+
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/only_list/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/lists/only_list/')         
+
+        self.assertContains(response, 'item 1')
+        self.assertContains(response, 'item 2')
