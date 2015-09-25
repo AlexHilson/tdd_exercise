1bff56cdf943d96879d4fe1fb9aa714e89b98bb2
Alex Hilson <alexhilsonwork@googlemail.com>
2015-09-25 15:10:07 +0100

    Modified homepage to store POSTs as Item and render all Items on GET.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -8,28 +8,55 @@ from lists.models import Item
 
 
 class HomePageTest(TestCase):
+    def setup_post(self, item_text):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = item_text
+        return request
+
     def test_root_url_resolves_to_homepage(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
-        
+
     def test_homepage_returns_correct_html(self):
         request = HttpRequest()
-        
+
         response = home_page(request)
         expected_html = render_to_string('home.html')
-        
+
         self.assertEqual(response.content.decode(), expected_html)
-        
-    def test_homepage_saves_post_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-        
+
+    def test_homepage_saves_POST_request(self):
+        request = self.setup_post('A new list item')
+
         response = home_page(request)
-        expected_html = render_to_string(
-            'home.html', 
-            {'new_item_text': 'A new list item'})
-        self.assertEqual(response.content.decode(), expected_html)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_hompage_redirects_after_POST(self):
+        request = self.setup_post('A new list item')
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_homepage_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_homepage_displays_all_todos(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('item 1', response.content.decode())
+        self.assertIn('item 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -38,14 +65,14 @@ class ItemModelTest(TestCase):
         first_item = Item()
         first_item.text = 'The first list item'
         first_item.save()
-        
+
         second_item = Item()
         second_item.text = 'The second list item'
         second_item.save()
-        
+
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
-        
+
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first list item')
