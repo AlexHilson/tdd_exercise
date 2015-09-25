550c796f6e111363c36f7eab8744e5e5264aa3f7
Alex Hilson <alexhilsonwork@googlemail.com>
2015-09-25 16:33:28 +0100

    Created List model, and updated Item model so that each Item is associated with a List

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -21,16 +21,24 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
         # Overly verbose to introduce Django ORM
+        new_list = List()
+        new_list.save()
+
         first_item = Item()
         first_item.text = 'The first list item'
+        first_item.list = new_list
         first_item.save()
 
         second_item = Item()
         second_item.text = 'The second list item'
+        second_item.list = new_list
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(new_list, saved_list)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -38,7 +46,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first list item')
+        self.assertEqual(first_saved_item.list, new_list)
         self.assertEqual(second_saved_item.text, 'The second list item')
+        self.assertEqual(second_saved_item.list, new_list)
 
 
 class ListViewTest(TestCase):
@@ -47,8 +57,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
+        new_list = List.objects.create()
+        Item.objects.create(text='item 1', list=new_list)
+        Item.objects.create(text='item 2', list=new_list)
 
         response = self.client.get('/lists/only_list/')         
 
